c69c7bd5ed93f3983f512c298a18091632f41651
MUKILTEO\1717998 <1717998@mukilteo.wednet.edu>
2024-03-26 10:07:24 -0700

    Did more on Button UI I think I got it almost done just need to figure some things out before being done

--- Assets/Ruth/ButtonUI.cs
+++ Assets/Ruth/ButtonUI.cs
@@ -1,22 +1,66 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonUI : MonoBehaviour
 {
     private AudioSource buttonPress;
 
     [SerializeField]
+    private AudioSource music;
+
+    [SerializeField]
     private AudioSource exitButton;
 
-    private void Start()
+    public void Start()
     {
         buttonPress = GetComponent<AudioSource>();
     }
 
-    // Global
     IEnumerator GoToScene(string Scene)
     {
-        
+        AudioSource audio;
+
+        if (Scene == "Quit")
+            audio = exitButton;
+        else 
+            audio = buttonPress;
+
+        audio.Play();
+        music.Stop();
+       
+
+        while (audio.isPlaying)
+        {
+            yield return null;
+        }
+
+        if (Scene == "Quit")
+            Application.Quit();
+        else 
+            SceneManager.LoadScene(Scene);
+
+    }
+
+    // Buttons
+
+    public void Play()
+    {
+        StartCoroutine(GoToScene("Play"));
+    }
+
+
+    public void Quit()
+    {
+        StartCoroutine(GoToScene("MainMenu"));
     }
 }
+
+
+
+
+//      skibidi!!!!!!!!!!!!!
+
