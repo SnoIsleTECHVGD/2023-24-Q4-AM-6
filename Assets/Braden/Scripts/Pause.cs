fb56b616c251777d7227fca8558c5621a4d31533
MUKILTEO\1719263 <1719263@mukilteo.wednet.edu>
2024-04-15 10:14:04 -0700

    begin pausing the paused pausing of 1987

---
+++ Assets/Braden/Scripts/Pause.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public bool isPaused = false;
+    public KeyCode pauseKey = KeyCode.Escape;
+
+    public GameObject mainCanvas;
+    public GameObject bgCanvas;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+
+        mainCanvas.SetActive(false);
+        bgCanvas.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(pauseKey))
+            SetPaused(!isPaused);
+    }
+
+    // Main
+
+    public void SetPaused(bool state)
+    {
+        if (state == isPaused)
+            return;
+
+        isPaused = state;
+
+        mainCanvas.SetActive(state);
+        bgCanvas.SetActive(state);
+
+        if (state == true)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }
+
+    public void SwitchToMenu()
+    {
+        SceneManager.LoadScene("mainmenu", LoadSceneMode.Single);
+    }
+}
