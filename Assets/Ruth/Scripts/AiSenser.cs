a2c677e9cc9ffe81cd8bb49327baba7afde1e22e
MUKILTEO\1717998 <1717998@mukilteo.wednet.edu>
2024-04-25 10:04:01 -0700

    working on the enemy sight light wtv thing

--- Assets/Ruth/Scripts/AiSenser.cs
+++ Assets/Ruth/Scripts/AiSenser.cs
@@ -24,11 +24,17 @@ public class AiSenser : MonoBehaviour
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
 
-        
+
 
         if (distance < distanceBetween)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
         }
+        //    GetComponentInParent<Enemies>().enabled = false;
+        //}
+        //else
+        //{
+        //    GetComponentInParent<Enemies>().enabled = true;
+        //}
     }
 }
